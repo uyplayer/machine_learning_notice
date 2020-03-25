07d83d69e279d180c4b1b77781f4738fe848ffec
uyplayer <32848979@qq.com>
2020-03-25 20:11:36 +0800

    'word2vec'

---
+++ nlp/Word2Vec-Skipgram.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/25 17:28
+# @Author  : uyplayer
+# @Site    : uyplayer.xyz
+# @File    : Word2Vec-Skipgram.py
+# @Software: PyCharm
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+dtype = torch.FloatTensor
+
+#  Sentence
+sentences = [ "i like dog", "i like cat", "i like animal",
+              "dog cat animal", "apple cat dog like", "dog fish milk like",
+              "dog cat eyes like", "i like apple", "apple i hate",
+              "apple i movie book music like", "cat dog hate", "cat dog like"]
+# print(len(sentences)) # 12
+
+word_sequence = " ".join(sentences).split()
+word_list = " ".join(sentences).split()
+word_list = list(set(word_list))
+word_dict = {w: i for i, w in enumerate(word_list)}
+# print(len(word_list)) # 13
+
+# Word2Vec Parameter
+batch_size = 20  # To show 2 dim embedding graph
+embedding_size = 2  # To show 2 dim embedding graph
+voc_size = len(word_list)
+
+
+def random_batch(data, size):
+    random_inputs,random_labels = [],[]
+    random_index = np.random.choice(range(len(data)), size, replace=False) # random
+
+    for i in random_index:
+        # print(np.eye(voc_size))
+        # print(data[i][0])
+        # print(np.eye(voc_size)[data[i][0]])
+        # exit()
+        random_inputs.append(np.eye(voc_size)[data[i][0]])  # target
+        random_labels.append(data[i][1])  # context word
+
+    return random_inputs, random_labels
+
+# Make skip gram of one size window
+skip_grams = []
+for i in range(1, len(word_sequence) - 1):
+    target = word_dict[word_sequence[i]]
+    context = [word_dict[word_sequence[i - 1]], word_dict[word_sequence[i + 1]]]
+
+    for w in context:
+        skip_grams.append([target, w])
+
+# Model
+class Word2Vec(nn.Module):
+    def __init__(self):
+        super(Word2Vec,self).__init__()
+
+        self.W = nn.Parameter(-2 * torch.rand(voc_size, embedding_size) + 1).type(
+            dtype)  # voc_size > embedding_size Weight
+        self.WT = nn.Parameter(-2 * torch.rand(embedding_size, voc_size) + 1).type(
+            dtype)  # embedding_size > voc_size Weight
+
+    def forward(self,X):
+        hidden_layer  = torch.matmul(X,self.W)
+        output_layer = torch.matmul(hidden_layer, self.WT)
+        return output_layer
+
+model = Word2Vec()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(),lr=0.001)
+
+for epoch in range(500):
+    input_batch, target_batch = random_batch(skip_grams,batch_size)
+
+    input_batch = Variable(torch.Tensor(input_batch))
+    target_batch = Variable(torch.LongTensor(target_batch))
+
+    optimizer.zero_grad()
+    output = model(input_batch)
+
+    loss = criterion(output,target_batch)
+
+    if (epoch + 1) % 1000 == 0:
+        print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
+
+    loss.backward()
+    optimizer.step()
+
+
+for i, label in enumerate(word_list):
+    W, WT = model.parameters()
+    x,y = float(W[i][0]), float(W[i][1])
+    plt.scatter(x, y)
+    plt.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points', ha='right', va='bottom')
+plt.show()
+
+
+
+
+
+
+
+
