db847e789db7f1a3ffd85c8080360d445cec26bf
uyplayer <32848979@qq.com>
2020-12-01 20:54:24 +0800

    okk

--- pytorch/pytorch_with_examples.py
+++ pytorch/pytorch_with_examples.py
@@ -28,18 +28,18 @@ for t in range(500):
     h_relu = np.maximum(h,0) # if an element in array is small than 0 ; then 0 replaces this element
     y_pred = h_relu.dot(w2) # (64,100)*(100,10) = (64,10)
 
-    #loss
+    # loss
     loss = np.square(y_pred - y) # (64,10)
     loss = loss.sum() # a float number
     print(t, loss)
 
-    #backprop to compute gradients of w1 and w2 with respect to loss
-    grad_y_pred = 2.0 * (y_pred - y)
-    grad_w2 = h_relu.T.dot(grad_y_pred)
-    grad_h_relu = grad_y_pred.dot(w2.T)
+    # backprop to compute gradients of w1 and w2 with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y) # (64,10)
+    grad_w2 = h_relu.T.dot(grad_y_pred) # (64,100).T = (100,64); (100,64)*(64,10) = (100,10)
+    grad_h_relu = grad_y_pred.dot(w2.T) # (64,10)*(10,100) = (64,100)
     grad_h = grad_h_relu.copy()
     grad_h[h < 0] = 0
-    grad_w1 = x.T.dot(grad_h)
+    grad_w1 = x.T.dot(grad_h) # (1000,64)*(64,100) = (1000,100)
 
     # Update weights
     w1 -= learning_rate * grad_w1
